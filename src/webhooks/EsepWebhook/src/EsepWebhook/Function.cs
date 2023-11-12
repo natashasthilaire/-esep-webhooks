983e2d8fcfc785bbcd26e49d70209ad977f5e1c7
Natasha St Hilaire <natashasthilaire@ufl.edu>
2023-11-12 00:15:58 -0500

    added code

--- src/webhooks/EsepWebhook/src/EsepWebhook/Function.cs
+++ src/webhooks/EsepWebhook/src/EsepWebhook/Function.cs
@@ -18,6 +18,15 @@ public class Function
     {
         dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-        return input.ToUpper();
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+
+        return reader.ReadToEnd();
+
     }
 }
